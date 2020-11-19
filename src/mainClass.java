e9ea1087ba9061a2e384e5eebd88bdfa6daf5301
HassanAhmed25 <hassanahmed2569@gmail.com>
2020-11-19 18:51:23 +0500

    Add files via upload

---
+++ src/mainClass.java
@@ -0,0 +1,148 @@
+import java.util.Scanner;
+
+public class mainClass {
+    public static void main(String[] args) {
+
+        char choice;
+        Scanner keyboard = new Scanner(System.in);
+        int[] someArray;
+        System.out.print("How many elements to store?: ");
+        int size = keyboard.nextInt();
+        someArray = new int[size];
+        do {
+            System.out.println();
+            System.out.println("[1] Enter values");
+            System.out.println("[2] Find maximum");
+            System.out.println("[3] Calculate sum");
+            System.out.println("[4] Check membership");
+            System.out.println("[5] Search array");
+            System.out.println("[6] Display values");
+            System.out.println("[7] Calculate Average");
+            System.out.println("[8] Exit");
+            System.out.print("Enter choice [1-7]: ");
+            choice = keyboard.next().charAt(0);
+            System.out.println();
+            switch (choice) {
+                case '1':
+                    fillArray(someArray);
+                    break;
+                case '2':
+                    int max = max(someArray);
+                    System.out.println("Maximum array value = " + max);
+                    break;
+                case '3':
+                    int total = sum(someArray);
+                    System.out.println("Sum of array values = " + total);
+                    break;
+                case '4':
+                    System.out.print("Enter value to find: ");
+                    int value = keyboard.nextInt();
+                    boolean found = contains(value, someArray);
+                    if (found) {
+                        System.out.println(value + " is in the array");
+                    } else {
+                        System.out.println(value + " is not in the array");
+                    }
+                    break;
+                case '5':
+                    System.out.print("Enter value to find: ");
+                    int item = keyboard.nextInt();
+                    int index = search(item,someArray);
+                    if (index == -999) // indicates value not found
+                    {
+                        System.out.println("This value is not in the array");
+                    } else {
+                        System.out.println("This value is at array index " + index);
+                    }
+                    break;
+                case '6':
+                    System.out.println("Array values");
+                    displayArray(someArray);
+                    break;
+
+                case '7':
+                    System.out.println("Average " + getAvg(someArray));
+                    break;
+            }
+        } while (choice != '8');
+        System.out.println("Goodbye");
+
+    }
+
+    static void fillArray(int... arrayIn)
+    {
+        Scanner keyboard = new Scanner(System.in);
+        for (int i = 0; i < arrayIn.length; i++)
+        {
+            System.out.print("enter value ");
+            arrayIn[i] = keyboard.nextInt();
+        }
+    }
+
+    static int sum (int... arrayIn)
+    {
+        int total = 0;
+        for (int i = 0 ; i < arrayIn.length; i++)
+        {
+            total = total + arrayIn[i];
+        }
+        return total;
+    }
+
+    static int max (int... arrayIn)
+    {
+        int result = arrayIn[0]; // set result to the first value in the array
+// this loops runs from the 2nd item to the last item in the array
+        for (int i=1; i < arrayIn.length; i++)
+        {
+            if (arrayIn[i] > result)
+            {
+                result = arrayIn[i]; // reset result to new maximum
+            }
+        }
+        return result;
+    }
+
+    static boolean contains (int valueIn, int... arrayIn)
+    {
+// enhanced 'for' loop used here
+        for (int currentElement : arrayIn)
+        {
+            if (currentElement == valueIn)
+            {
+                return true; // exit loop early if value found
+            }
+        }
+        return false; // value not present
+    }
+
+    static int search (int valueIn, int... arrayIn)
+    {
+        for (int i = 0; i < arrayIn.length; i++)
+        {
+            if (arrayIn[i] == valueIn)
+            {
+                return i;
+            }
+        }
+        return -999;
+    }
+
+    static void displayArray(int... arrayIn)
+    {
+        System.out.println();
+// standard 'for' loop used here as the array index is required
+        for (int i = 0; i < arrayIn.length; i++)
+        {
+            System.out.println("array[" + i + "] = " + arrayIn[i]);
+        }
+    }
+
+    static int getAvg(int... arrayIn)
+    {
+        int sum = sum(arrayIn);
+        int total = arrayIn.length;
+        int avg = sum/total;
+        return avg;
+    }
+}
